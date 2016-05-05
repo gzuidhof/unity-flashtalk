2004c58724a619602621887d993801fa55fc8b28
Guido <me@guido.io>
2016-05-06 01:17:25 +0200

    Hue, eyes juice

---
+++ Slaim/Assets/Juice/HueModulator.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HueModulator : MonoBehaviour {
+
+    private Camera _camera;
+    private SpriteRenderer _rend;
+
+    private float startH;
+    private float startV;
+    private float startS;
+
+    public float speed = 60f;
+
+	// Use this for initialization
+	void Start () {
+
+        Color startColor = new Color();
+
+        _camera = gameObject.GetComponent<Camera>();
+        
+        _rend = gameObject.GetComponent<SpriteRenderer>();
+        if (_rend) {
+            startColor = _rend.color;
+        }
+
+        if (_camera)
+        {
+            startColor = _camera.backgroundColor;
+        }
+
+
+        Util.RGBToHSV(startColor, out startH, out startV, out startS);
+    }
+	
+    Color hueShift()
+    {
+        startH += Time.deltaTime * speed;
+
+        return Util.HsvToRgbColor(startH, startS, startV);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+
+
+        if (_camera)
+        {
+            _camera.backgroundColor = hueShift();
+        }
+        else if (_rend)
+        {
+            
+            _rend.color =hueShift();// = hueShift();
+        }
+
+	}
+}
---
+++ Slaim/Assets/Juice/Util.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Util
+{
+
+    public static Color HsvToRgbColor(double h, double s, double v)
+    {
+        int r, g, b;
+        HsvToRgb(h, s, v, out r, out g, out b);
+        return new Color(r / 255f, g / 255f, b / 255f);
+    }
+
+    public static void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
+    {
+        // ######################################################################
+        // T. Nathan Mundhenk
+        // mundhenk@usc.edu
+        // C/C++ Macro HSV to RGB
+
+        double H = h;
+        while (H < 0) { H += 360; };
+        while (H >= 360) { H -= 360; };
+        double R, G, B;
+        if (V <= 0)
+        { R = G = B = 0; }
+        else if (S <= 0)
+        {
+            R = G = B = V;
+        }
+        else
+        {
+            double hf = H / 60.0;
+            int i = (int)Math.Floor(hf);
+            double f = hf - i;
+            double pv = V * (1 - S);
+            double qv = V * (1 - S * f);
+            double tv = V * (1 - S * (1 - f));
+            switch (i)
+            {
+
+                // Red is the dominant color
+
+                case 0:
+                    R = V;
+                    G = tv;
+                    B = pv;
+                    break;
+
+                // Green is the dominant color
+
+                case 1:
+                    R = qv;
+                    G = V;
+                    B = pv;
+                    break;
+                case 2:
+                    R = pv;
+                    G = V;
+                    B = tv;
+                    break;
+
+                // Blue is the dominant color
+
+                case 3:
+                    R = pv;
+                    G = qv;
+                    B = V;
+                    break;
+                case 4:
+                    R = tv;
+                    G = pv;
+                    B = V;
+                    break;
+
+                // Red is the dominant color
+
+                case 5:
+                    R = V;
+                    G = pv;
+                    B = qv;
+                    break;
+
+                // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
+
+                case 6:
+                    R = V;
+                    G = tv;
+                    B = pv;
+                    break;
+                case -1:
+                    R = V;
+                    G = pv;
+                    B = qv;
+                    break;
+
+                // The color is not defined, we should throw an error.
+
+                default:
+                    //LFATAL("i Value error in Pixel conversion, Value is %d", i);
+                    R = G = B = V; // Just pretend its black/white
+                    break;
+            }
+        }
+        r = Clamp((int)(R * 255.0));
+        g = Clamp((int)(G * 255.0));
+        b = Clamp((int)(B * 255.0));
+    }
+
+    /// <summary>
+    /// Clamp a value to 0-255
+    /// </summary>
+    static int Clamp(int i)
+    {
+        if (i < 0) return 0;
+        if (i > 255) return 255;
+        return i;
+    }
+
+
+
+
+
+
+    public static void RGBToHSV(Color rgbColor, out float H, out float S, out float V)
+    {
+        if (rgbColor.b > rgbColor.g && rgbColor.b > rgbColor.r)
+        {
+            RGBToHSVHelper(4f, rgbColor.b, rgbColor.r, rgbColor.g, out H, out S, out V);
+        }
+        else
+        {
+            if (rgbColor.g > rgbColor.r)
+            {
+                RGBToHSVHelper(2f, rgbColor.g, rgbColor.b, rgbColor.r, out H, out S, out V);
+            }
+            else
+            {
+                RGBToHSVHelper(0f, rgbColor.r, rgbColor.g, rgbColor.b, out H, out S, out V);
+            }
+        }
+    }
+
+    private static void RGBToHSVHelper(float offset, float dominantcolor, float colorone, float colortwo, out float H, out float S, out float V)
+    {
+        V = dominantcolor;
+        if (V != 0f)
+        {
+            float num = 0f;
+            if (colorone > colortwo)
+            {
+                num = colortwo;
+            }
+            else
+            {
+                num = colorone;
+            }
+            float num2 = V - num;
+            if (num2 != 0f)
+            {
+                S = num2 / V;
+                H = offset + (colorone - colortwo) / num2;
+            }
+            else
+            {
+                S = 0f;
+                H = offset + (colorone - colortwo);
+            }
+            H /= 6f;
+            if (H < 0f)
+            {
+                H += 1f;
+            }
+        }
+        else
+        {
+            S = 0f;
+            H = 0f;
+        }
+    }
+}
