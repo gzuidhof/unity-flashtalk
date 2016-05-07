3ed04e40f1e0e4e76e28140d7c0d28fc3fc7c733
Guido <me@guido.io>
2016-05-07 04:31:55 +0200

    Hitmarkers

---
+++ Slaim/Assets/Juice/Hitmarker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hitmarker : MonoBehaviour {
+
+    public GameObject hitmarkerPrefab;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Player")) {
+            Vector2 position = coll.contacts[0].point;
+            var go = GameObject.Instantiate(hitmarkerPrefab, position, Quaternion.identity);
+            Destroy(go, 0.235f);
+        }
+    }
+}
